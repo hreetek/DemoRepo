4968e39e999152839ec219ee8f328d941bf3acd8
Kumar Lav <kumarstslav@gmail.com>
2019-05-10 16:04:18 +0530

    Second Comitt

--- DemoRepoo/DemoRepoo/ViewController.swift
+++ DemoRepoo/DemoRepoo/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmnt: UISegmentedControl!
+    @IBOutlet weak var btn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("inside View DidLoad............")
     }
 
-
+    @IBAction func btnaction(_ sender: Any) {
+   
+    
+    
+    }
+    
+    
+    @IBAction func segaction(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
 }
 
